1538b0d65449134431910f7fa1cac7163cf0de9d
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2024-03-25 09:12:22 +0900

    upload gitignore

---
+++ SendNow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendNow
+//
+//  Created by 한소희 on 3/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
